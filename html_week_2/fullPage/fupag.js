8cf21722ba06a38daf88baa517af538d91f0fc84
unknown <JF.Gallegos@FRBI094915C1774.frccinst.ccofc.edu>
2015-04-08 21:02:52 -0600

    ffffunWithBoxesandFlexBox

---
+++ html_week_2/fullPage/fupag.js
@@ -0,0 +1,7 @@
+/**
+ * Created by JF.Gallegos on 4/8/2015.
+ */
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
